ab0c75c3ea4fd911ddaf6c0a3f246463e0308b21
Daphne Augier <35980232+daphneaugier@users.noreply.github.com>
2021-10-05 23:38:49 -0400

    db: populated db tables

--- migrations/Version20211006023725.php
+++ migrations/Version20211006023725.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211006014551 extends AbstractMigration
+final class Version20211006023725 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20211006014551 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment CHANGE comment_text commentÂ_text LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE posts CHANGE updated_at updated_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE comment CHANGE commentÂ_text comment_text LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE posts CHANGE updated_at updated_at DATETIME NOT NULL');
     }
 }
