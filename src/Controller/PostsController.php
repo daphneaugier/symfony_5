4e6a126d9d517de37169ef6312644a978116a2c9
Daphne Augier <35980232+daphneaugier@users.noreply.github.com>
2021-10-08 13:31:21 -0400

    feat: added read() to open posts on single page

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,25 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Posts;
 use App\Repository\PostsRepository;
+use Doctrine\ORM\Mapping\Driver\RepeatableAttributeCollection;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PostsController extends AbstractController
+class PostsController  extends AbstractController
 {
     /**
      * @Route("/", name="posts")
      */
-
     public function index(PostsRepository $postsRepository): Response
-    {
+        {
         // $posts = $postsRepository->findAll();
         // dd($posts);
 
-        $lastPosts = $postsRepository->findLastPosts(6);
-        return $this->render('posts/index.html.twig', [
-            'lastPosts' => $lastPosts,
+            $lastPosts = $postsRepository->findLastPosts(6);
+            return $this->render('posts/index.html.twig', ['lastPosts' => $lastPosts,
+            ]);
+
+    }
+
+    /**
+     * Display of selected post
+     * @Route("/posts/{id}", name="posts_read")
+     *
+     * @return void
+     */
+    public function read(Posts $posts): Response
+    {
+        return $this->render('posts/read.html.twig', [
+            'posts' => $posts,
         ]);
     }
+
 }
