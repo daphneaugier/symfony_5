547834e2818db4d509427a20892078be509abf0e
Daphne Augier <35980232+daphneaugier@users.noreply.github.com>
2021-10-10 15:37:27 -0400

    feat: added artists controller/page

---
+++ src/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/artists", name="users")
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('user/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
