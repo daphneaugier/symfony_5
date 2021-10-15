cf5ea34bd094873c01208a69af421ed63ed5a745
Daphne Augier <35980232+daphneaugier@users.noreply.github.com>
2021-10-15 19:58:25 -0400

    feat: submit section to add an artwork with image and text

--- src/Controller/ArtworksController.php
+++ src/Controller/ArtworksController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\ArtworkType;
 use App\Entity\Posts;
 use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,5 +22,61 @@ class ArtworksController extends AbstractController
             'publishedPosts' => $publishedPosts,
         ]);
 
+    }
+
+    /**
+     * @Route("/submit", name="submit")
+     */
+    public function add(Request $request): Response
+    {
+        if($this->getUser() == null){
+            return $this->redirectToRoute('posts');
+        }else{
+            $new_post = new Posts();
+            $new_post->setUser($this->getUser());
+            $new_post->setCreatedAt(new \DateTime());
+            $new_post->setUpdatedAt(new \DateTime());
+
+            $form = $this->createForm(ArtworkType::class, $new_post);
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $image_file = $form->get('image_file')->getData();
+
+                if ($image_file) {
+                    $originalFilename = pathinfo($image_file->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename.'-'.uniqid().'.'.$image_file->guessExtension();
+
+                    // Move the file to the directory where images are stored
+                    try {
+                        $image_file->move(
+                            $this->getParameter('images_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        echo "Image : $image_file error $e";
+                        die();
+                    }
+
+                    // updates the 'image_filename' property to store the image file name
+                    // instead of its contents
+                    $new_post->setImage($newFilename);
+
+                    $new_post = $form->getData();
+
+                    $entityManager = $this->getDoctrine()->getManager();
+                    $entityManager->persist($new_post);
+                    $entityManager->flush();
+
+                    return $this->redirectToRoute('posts');
+                }
+            }
+
+            return $this->renderForm('artworks/submit.html.twig', [
+                'form' => $form,
+            ]);
+        }
     }
 }
---
+++ src/Form/ArtworkType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Posts;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\File;
+
+class ArtworkType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('image_file', FileType::class, [
+                'label' => 'Image',
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2048k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image document file',
+                    ])
+                ],
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Title'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Content'
+            ])
+            ->add('status', ChoiceType::class, [
+                'choices'  => [
+                    'Published' => 'PUBLISHED',
+                    'Draft' => 'DRAFT',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn-dark'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Posts::class,
+        ]);
+    }
+}
